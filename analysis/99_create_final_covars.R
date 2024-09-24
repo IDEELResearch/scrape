20ee260964439880cec2dbae77a35ff3ad484a82
OJWatson <o.watson15@imperial.ac.uk>
2024-09-24 12:58:11 +0100

    Update final covariates with Milestone 2 outputs

--- analysis/99_create_final_covars.R
+++ analysis/99_create_final_covars.R
@@ -5,10 +5,10 @@ library(tidyverse)
 # ---------------------------------------------------- #
 
 # Covariates
-prev <- readRDS("analysis/data-derived/pfpr210_2010-2022.rds")
+prev <- readRDS("analysis/data-derived/pfpr210_2000-2022.rds")
 seasonality <- readRDS("analysis/data-derived/seasonality.rds")
-act <- readRDS("analysis/data-derived/ACT_usage_2010-2022.rds")
-ft <- readRDS("analysis/data-derived/ft_2010-2022.rds")
+act <- readRDS("analysis/data-derived/ACT_usage_2000-2022.rds")
+ft <- readRDS("analysis/data-derived/ft_2000-2022.rds")
 
 # Map to bind to
 admin1 <- readRDS("analysis/data-derived/admin1_sf.rds")
@@ -26,7 +26,7 @@ create_final_df <- function(yr){
     left_join(ft %>% filter(year == yr)) %>% 
     left_join(act %>% filter(year == yr)) %>% 
     sf::st_drop_geometry() %>% 
-    select(iso3c, name_0, id_0, name_1, id_1, year, pfpr210_low:ASPY) %>% 
+    select(iso3c, name_0, id_0, name_1, id_1, year, pfpr210:ASPY) %>% 
     as.tibble()
   
 }
@@ -36,7 +36,7 @@ create_final_df <- function(yr){
 # ---------------------------------------------------- #
 
 # pull for each year
-yr_range <- 2010:2022
+yr_range <- 2000:2022
 res <- lapply(yr_range, create_final_df)
 names(res) <- paste0("Y", yr_range)
 
