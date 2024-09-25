2627254df22c7b1a774abc510cb5e46d6c27a71a
OJWatson <o.watson15@imperial.ac.uk>
2024-09-25 04:38:04 +0100

    update final covariates

--- analysis/99_create_final_covars.R
+++ analysis/99_create_final_covars.R
@@ -7,9 +7,10 @@ library(tidyverse)
 # Covariates
 prev <- readRDS("analysis/data-derived/pfpr210_2000-2022.rds")
 seasonality <- readRDS("analysis/data-derived/seasonality.rds")
-travel <- readRDS("analysis/data-derived/travel.rds")
+travel <- readRDS("analysis/data-derived/MAP_travel.rds")
 act <- readRDS("analysis/data-derived/ACT_usage_2000-2022.rds")
 ft <- readRDS("analysis/data-derived/subnational_ft_2000-2022.rds")
+pop <- readRDS("analysis/data-derived/pop_2000-2022.rds")
 
 # Map to bind to
 admin1 <- readRDS("analysis/data-derived/admin1_sf.rds")
@@ -22,12 +23,14 @@ admin1 <- readRDS("analysis/data-derived/admin1_sf.rds")
 create_final_df <- function(yr){
   
   left_join(admin1 %>% rename(iso3c = iso), 
-            prev %>% filter(year == yr)) %>% 
-    left_join(seasonality) %>% 
+            seasonality) %>% 
+    left_join(travel) %>% 
+    left_join(prev %>% filter(year == yr)) %>% 
     left_join(ft %>% filter(year == yr)) %>% 
     left_join(act %>% filter(year == yr)) %>% 
+    left_join(pop %>% filter(year == yr)) %>% 
     sf::st_drop_geometry() %>% 
-    select(iso3c, name_0, id_0, name_1, id_1, year, pfpr210:ASPY) %>% 
+    select(iso3c, name_0, id_0, name_1, id_1, year, seasonality:pop_total) %>% 
     as.tibble()
   
 }
